6ffc91660ff188ec4314f1955527e1f512a5ba47
Czeluw <churu.zelaya@gmail.com>
2020-05-03 12:43:29 -0600

    desprin

--- JS/Homepage.js
+++ JS/Homepage.js
@@ -1,2 +1,13 @@
+//out the last modified date on main page
 let lastDate = document.lastModified;
 document.getElementById("upTime").innerHTML = lastDate;
+
+//function to open navigation bar
+function openNav() {
+    document.getElementById("boopbar").style.width = "250px";
+}
+  
+//function to close navigation bar
+function closeNav() {
+    document.getElementById("boopbar").style.width = "0";
+}
