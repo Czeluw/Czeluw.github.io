658130f30612d5625abdb7cc6903543424927901
Czeluw <churu.zelaya@gmail.com>
2020-05-21 12:26:59 -0600

    fixed css

--- Lesson4/JS/preston.js
+++ Lesson4/JS/preston.js
@@ -1,4 +1,4 @@
-//Toggle menu open-close
+/*Toggle menu open-close
 var windowWidth = $(window).width();
 
 $("#nav-toggle").click(function () {
@@ -48,3 +48,20 @@ const monthName = months[d.getMonth()];
 const dayName = days[d.getDay()];
 const formatted = `${dayName}, ${date} ${monthName} ${year}`;
 document.getElementById("upTime").innerHTML = formatted;
+*/
+
+//Hamburger menu
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('.ham');
+
+hambutton.addEventListener('click', 
+() => {mainnav.classList.toggle('responsive')}, false);
+
+//custom date
+let weekDay = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let monthYear = ['January','February', 'March', 'April', 'May','June', 'July', 'August', 'September', 'October', 'November', 'December'];
+let mydate = new Date();
+let weekname = weekDay[mydate.getDay()];
+let monthname  = monthYear[mydate.getMonth()];
+const dateoutput = document.querySelector('.theTime');
+dateoutput.textContent = weekname + ', ' + mydate.getDate() + " " + monthname + " " + mydate.getFullYear();
